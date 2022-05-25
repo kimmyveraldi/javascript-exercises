100baf5cf0e611e207634ceb821d914951ccda19
Kimmy Veraldi <kimveraldi@gmail.com>
2022-05-25 19:54:13 -0400

    07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+const ftoc = function (temp) {
+  //subtract 32 and multiply by (5/9)
 
+  return (Math.round(((temp - 32) * (5/9)) * 10) / 10)
 };
 
-const ctof = function() {
+const ctof = function (temp) {
+  //multiply by (9/5) plus 32
 
+  return (Math.round(((temp * 9/5) + 32) * 10) / 10)
 };
 
 // Do not edit below this line
