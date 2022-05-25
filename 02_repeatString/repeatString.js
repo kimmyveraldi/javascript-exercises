3928b192efb9a70b4c1d88f9a140c34f3b8aff69
Kimmy Veraldi <kimveraldi@gmail.com>
2022-05-25 16:43:39 -0400

    02 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+  if (num<0) return "ERROR";
+  multipliedString=''
+  for(i=0;i<num;i++) {
+    multipliedString += string;
+  }
+  return(multipliedString);
 };
 
 // Do not edit below this line
