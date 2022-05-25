6894029747d56ba053938df1da169d1b1e513ddf
Kimmy Veraldi <kimveraldi@gmail.com>
2022-05-25 17:25:27 -0400

    04 complete using filter and arrow function. Test for values not include in the list of arguments and keep them in array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
-};
+var removeFromArray = function(...args) {
+    const originalArray = args[0];
+    let resultArray = originalArray.filter(item => !args.includes(item));
+    return resultArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
