4a525a6541274fd80ab0c567946b0147193c25ac
Kimmy Veraldi <kimveraldi@gmail.com>
2022-05-25 19:02:15 -0400

    05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
-};
+const sumAll = function (value1, value2) {
+    let sum = 0;
+    let intArray = [value1, value2].sort()
+    
+    if (value1 > 0 && value2 > 0 && Number.isInteger(value1) && Number.isInteger(value2)) {
+        for (i = intArray[0]; i < intArray[1] + 1; i++) {
+            sum += i;
+        }
+        return (sum);
+    }
+    else {
+        return ("ERROR");
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
