9ae7d0daddce5545fff93b94f7bc2093987d72b5
Kimmy Veraldi <kimveraldi@gmail.com>
2022-05-25 19:34:50 -0400

    06 complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+// Leap year rules: divisible by 4, 100 and 400 
+const leapYears = function (year) {
+    return (Number.isInteger(year / 4) && ( !Number.isInteger(year / 100) || Number.isInteger(year / 400)))
 };
 
 // Do not edit below this line
