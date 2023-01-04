0ecb5deaa35fc457d680dd30329085cf36f58b06
Kimmy Veraldi <kimveraldi@gmail.com>
2023-01-04 16:45:41 -0500

    08 Complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+  return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total,num) => total+num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total,num) => total*num, 1);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x,y);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let total = 1;
+  if (n === 0 || n===1) return 1;
+  for (let i=n; i>0; i--) {
+    total = total * i;
+  }
+ return total;
 };
 
 // Do not edit below this line
