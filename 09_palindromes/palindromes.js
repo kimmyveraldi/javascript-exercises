f6bd315ff0ccf8f014ec7df3d9dc627addad5044
Kimmy Veraldi <kimveraldi@gmail.com>
2023-01-04 16:47:36 -0500

    initial commit

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+// const palindromes = function (str) {
+//     reversedstr = str.split("").reverse().join("");
+//     return reversedstr == str;
+// };
 
-};
+const palindromes = (str) => {
+    str = str.toLowerCase()
+             .replace(/[^a-z]/g, "");
+    reveresedstr = str.split("")
+                      .reverse().join("");
+
+    return reveresedstr == str;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
