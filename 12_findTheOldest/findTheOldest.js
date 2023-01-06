5a55297f2946eebaebe25a74efca9fa368d88e42
Kimmy Veraldi <kimveraldi@gmail.com>
2023-01-06 13:01:36 -0500

    12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((current,next) => {
+        const currentAge = age(current.yearOfDeath, current.yearOfBirth);
+        const nextAge = age(next.yearOfDeath, next.yearOfBirth);
+        return currentAge < nextAge ? next : current;
+    })
 };
+
+    const age = function(deathYear, birthYear) {
+        if (!deathYear) {
+            deathYear = new Date().getFullYear();
+        }
+        return (deathYear-birthYear);
+};
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
