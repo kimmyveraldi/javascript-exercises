4c6e0ad87b75e6311ed6fb8e6d5a2f175a787b76
Kimmy Veraldi <kimveraldi@gmail.com>
2023-01-06 01:37:24 -0500

    10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (n<0) return "OOPS";
+    if (n<3) return n==0 ? 0:1
+
+    let a=0, b=1, c=0;
+    while (--n) 
+        c = a+b, a=b, b=c;
+    return c;
 };
 
 // Do not edit below this line
